91fbb1de59c2664bd17930cccccb35a231768cf7
Ryan Crum <rcrum@crumcasa.com>
2016-03-20 22:24:31 -0400

    cleanup

--- www/public_html/include/gettheme.php
+++ www/public_html/include/gettheme.php
@@ -11,7 +11,19 @@ $theme = $sth2->fetchColumn();
 
 echo "<script src='/js/highcharts/themes/";echo $theme; echo ".js'></script>";
 
- 
+#Set default colors, since every chart pulls from this file
+
+$color_hivetemp="#FF1493"; #Green #FF1493 008000
+$color_hivehum="#0000FF"; #Blue
+$color_outtemp="#8A2BE2"; #Blue Vilolet
+$color_outhum="#000080"; #Navy
+$color_grossweight="#FFFF00"; #Yellow 
+$color_netweight="#FFD700"; #Gold
+$color_lux="#800000"; #Maroon
+$color_solarradiation="#FF0000"; #red
+$color_rain="#00FFFF"; #Aqua
+$color_gdd="#8B4513"; #SaddleBrown
+
 
 ?>
 
