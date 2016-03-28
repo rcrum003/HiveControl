4ff239ad94122fe52fed01acfdfd89bdc0153911
Ryan Crum <rcrum@crumcasa.com>
2016-03-27 23:11:48 -0400

    no message

---
+++ scripts/system/checkupgrades.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Check for upgrades
+# Only running once a day, when we load GDD
+
+#Check our upgrade script
+#Get the current version
+Upgrade_ver=$(cat /home/HiveControl/upgrade.sh | grep "Upgrade_ver" |awk -F\" '{print $2}')
+	#Get the version available
+	Upgrade_latest_ver=$(curl -s https://raw.githubusercontent.com/rcrum003/HiveControl/master/upgrade.sh |grep "Upgrade_ver" |awk -F\" '{print $2}')
+
+	if [[ $( echo "$Upgrade_ver < $Upgrade_latest_ver" | bc) -eq 1 ]]; then
+			curl -s https://raw.githubusercontent.com/rcrum003/HiveControl/master/upgrade.sh -o /home/HiveControl/upgrade.sh
+	fi
+
+
+#Get our current HiveControl Version
+Installed_Ver=$(cat /home/HiveControl/VERSION)
+echo "Installed $Installed_Ver"
+
+Db="/home/HiveControl/data/hive-data.db"
+#Get the latest HiveControl version
+Latest_Ver=$(curl -s https://raw.githubusercontent.com/rcrum003/HiveControl/master/VERSION)
+echo "Newest version is $Latest_Ver"
+
+if [[  $(echo "$Installed_Ver == $Latest_Ver" | bc) -eq 1 ]]; then
+		echo "Nothing to do, you are at the latest version"
+		sqlite3 $Db "UPDATE hiveconfig SET upgrade_available=no WHERE id=1"
+		exit
+fi
+if [[  $(echo "$Installed_Ver > $Latest_Ver" | bc) -eq 1 ]]; then
+		echo "Hey, you are running a newer version that we have in the repository! - Exiting"
+		sqlite3 $Db "UPDATE hiveconfig SET upgrade_available=no WHERE id=1"
+		exit
+fi
+echo "We have some upgrading to do!"
+sqlite $Db "UPDATE hiveconfig SET upgrade_available=yes WHERE id=1"
