e86175b42e97c0517eb749c834cef706c57fe8fe
Ryan Crum <rcrum@crumcasa.com>
2016-03-27 23:22:26 -0400

    no message

--- scripts/system/checkupgrades.sh
+++ scripts/system/checkupgrades.sh
@@ -9,6 +9,7 @@ Upgrade_ver=$(cat /home/HiveControl/upgrade.sh | grep "Upgrade_ver" |awk -F\" '{
 	Upgrade_latest_ver=$(curl -s https://raw.githubusercontent.com/rcrum003/HiveControl/master/upgrade.sh |grep "Upgrade_ver" |awk -F\" '{print $2}')
 
 	if [[ $( echo "$Upgrade_ver < $Upgrade_latest_ver" | bc) -eq 1 ]]; then
+			echo "Found a new version of upgrade.sh, downloading.."
 			curl -s https://raw.githubusercontent.com/rcrum003/HiveControl/master/upgrade.sh -o /home/HiveControl/upgrade.sh
 	fi
 
@@ -24,7 +25,7 @@ echo "Newest version is $Latest_Ver"
 
 if [[  $(echo "$Installed_Ver == $Latest_Ver" | bc) -eq 1 ]]; then
 		echo "Nothing to do, you are at the latest version"
-		sqlite3 $Db "UPDATE hiveconfig SET upgrade_available=no WHERE id=1"
+		sqlite3 $Db "UPDATE hiveconfig SET upgrade_available=\"no\" WHERE id=1"
 		exit
 fi
 if [[  $(echo "$Installed_Ver > $Latest_Ver" | bc) -eq 1 ]]; then
@@ -33,4 +34,4 @@ if [[  $(echo "$Installed_Ver > $Latest_Ver" | bc) -eq 1 ]]; then
 		exit
 fi
 echo "We have some upgrading to do!"
-sqlite $Db "UPDATE hiveconfig SET upgrade_available=yes WHERE id=1"
+sqlite $Db "UPDATE hiveconfig SET upgrade_available=\"yes\" WHERE id=1"
