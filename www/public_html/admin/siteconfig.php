835035844f79b247de33850c86265b1a6ab4c5a6
Ryan Crum <rcrum@crumcasa.com>
2016-04-01 15:38:40 -0400

    no message

--- www/public_html/admin/siteconfig.php
+++ www/public_html/admin/siteconfig.php
@@ -85,7 +85,7 @@ function test_input_allow_slash($data) {
         
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Settings - Instruments</h1>
+                    <h1 class="page-header">Settings - Site Config</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
