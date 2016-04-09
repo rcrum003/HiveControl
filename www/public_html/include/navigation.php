83a2b1381d676454d029e34ed269f12e123b1f50
Ryan Crum <rcrum@crumcasa.com>
2016-04-09 17:19:56 -0400

    Updated header to include hive name

--- www/public_html/include/navigation.php
+++ www/public_html/include/navigation.php
@@ -2,17 +2,65 @@
 
 <?PHP include($_SERVER["DOCUMENT_ROOT"] . "/include/db-connect.php");
 
-#Get Site Orient Option
+#Get Site Orient and Options
 $sth3 = $conn->prepare("SELECT SITE_ORIENT,HIVENAME,SITE_TYPE FROM hiveconfig");
 $sth3->execute();
 $result3 = $sth3->fetch(PDO::FETCH_ASSOC);
 
-
-#foreach($result as $r)"$r['precip_1hr_in']
-
 $orient = $result3['SITE_ORIENT'];
 $id = $result3['HIVENAME'];
 $SITE_TYPE = $result3['SITE_TYPE'];
+
+# =====================================================================
+# Set the header
+# =====================================================================
+ echo ' 
+    <head>
+    <style>
+    input {
+        max-width: 100%;
+    } 
+    </style>
+
+   <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Application to manage hive management tasks, and to report on various instruments.">
+    <meta name="author" content="Ryan Crum">
+
+    <title>'; echo $id; echo ' - Hive Control</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Timeline CSS -->
+    <link href="../dist/css/timeline.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+    
+
+    <!-- Custom Fonts -->
+    <link href="../bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesnt work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>';
+
+
+#=====================
+# Show either wide/or normal navigations
+# Note, two different sets below
+#=======================
 
 echo '<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
             <div class="navbar-header">
--- www/public_html/pages/index.php
+++ www/public_html/pages/index.php
@@ -34,59 +34,19 @@ if(isset($_GET["period"])) {
 ?>
 
 
-
 <!DOCTYPE html>
 <html lang="en">
 
 
+    <!-- Header and Navigation -->
+       <?PHP include "../include/navigation.php" ?>
+    <!-- /Navigation -->
 
-<head>
-<style>
-    input {
-        max-width: 100%;
-    } 
-</style>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Application to manage hive management tasks, and to report on various instruments.">
-    <meta name="author" content="Ryan Crum">
-
-    <title>Hive Control</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- MetisMenu CSS -->
-    <link href="../bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
-
-    <!-- Timeline CSS -->
-    <link href="../dist/css/timeline.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
-    
-
-    <!-- Custom Fonts -->
-    <link href="../bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<?php include "datawidgets/currentconditions.php"; ?>
 
 
     <div id="wrapper">
-
-        <!-- Navigation -->
-       <?PHP include "../include/navigation.php" ?>
-        <!-- /Navigation -->
+<?php include "datawidgets/currentconditions.php"; ?>
+        
 
             <div class="row">
                 <div class="col-lg-12">
